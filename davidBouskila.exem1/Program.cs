941ff98b80850e161e689c3fc8c36d2dbcbd3153
Davidbouskila <117008523+Davidbouskila@users.noreply.github.com>
2023-06-18 10:44:44 +0300

    Add files via upload

---
+++ davidBouskila.exem1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davidBouskila.exem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi exem 1");
+        }
+    }
+}
